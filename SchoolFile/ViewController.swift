f08845e0d604592de04517fc4c9afefe8d44f3cb
ARPIT SINGH <39374484+Arpit160399@users.noreply.github.com>
2020-11-05 13:19:56 +0530

    Initial Commit

---
+++ SchoolFile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchoolFile
+//
+//  Created by Arpit Singh on 05/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
